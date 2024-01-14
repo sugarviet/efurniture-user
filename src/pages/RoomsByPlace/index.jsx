2636ea07170116896902f2dd2c8cd922a068e1e8
unknown <vietdanghoang1705@gmail.com>
2024-01-14 16:41:40 +0700

    fix: re-locate function check slug

--- src/pages/RoomsByPlace/index.jsx
+++ src/pages/RoomsByPlace/index.jsx
@@ -1,12 +1,11 @@
 import { useParams } from "react-router-dom";
-import useCheckRightSlug from "./hooks/useCheckRightSlug";
 import NotFound from "@pages/NotFound";
+import { handleCheckSlugRoom } from "@utils/handleCheckSlugRoom";
 
 const RoomsByPlace = () => {
   const { slug } = useParams();
-  const { handleCheckRoom } = useCheckRightSlug();
 
-  if (!handleCheckRoom(slug)) return <NotFound />;
+  if (!handleCheckSlugRoom(slug)) return <NotFound />;
 
   return <main>RoomsByPlace</main>;
 };
