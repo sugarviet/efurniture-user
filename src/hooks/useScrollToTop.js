a53d8a3f25040af6dfb7f8c28d2903764287ab33
truonggiang0405 <vutruonggiang452002@gmail.com>
2024-01-17 15:11:53 +0700

    fix scroll to top

--- src/hooks/useScrollToTop.js
+++ src/hooks/useScrollToTop.js
@@ -1,21 +1,10 @@
-import { useEffect } from 'react';
 
 const useScrollToTop = () => {
-  useEffect(() => {
-    const handleScrollToTop = () => {
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-    };
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-    window.addEventListener('scrollToTop', handleScrollToTop);
-    return () => {
-      window.removeEventListener('scrollToTop', handleScrollToTop);
-    };
-  }, []);
-
-  return () => window.dispatchEvent(new Event('scrollToTop'));
-};
 
 export default useScrollToTop;
