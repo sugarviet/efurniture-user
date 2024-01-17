ed4bb8ba5ab53a9248df6e79a70af23ab7bc21d1
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-01-17 19:08:51 +0700

    replace hooks Scroll To Top

--- src/components/Footer/components/FooterTop/index.jsx
+++ src/components/Footer/components/FooterTop/index.jsx
@@ -1,16 +1,26 @@
-import useScrollToTop from '@hooks/useScrollToTop';
-
 function FooterTop() {
+  const handleScrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
-    return (
-        <section className='furniture-divided-bottom flex flex-col gap-8 sm:flex-row sm:justify-between items-center pb-8'>
-            <img className='h-[60px] sm:h-[80px]' src='/images/logo.svg'></img>
-            <button onClick={useScrollToTop} className='text-[0.6875rem] tracking-[0.16em] leading-[1.27] flex items-center gap-[6px]'>
-                <span className='furniture-link '>BACK TO TOP</span>
-                <img className='rotate-[-90deg]' src='/images/arrow-right-long.svg'></img>
-            </button>
-        </section>
-    )
+  return (
+    <section className="furniture-divided-bottom flex flex-col gap-8 sm:flex-row sm:justify-between items-center pb-8">
+      <img className="h-[60px] sm:h-[80px]" src="/images/logo.svg"></img>
+      <button
+        onClick={handleScrollToTop}
+        className="text-[0.6875rem] tracking-[0.16em] leading-[1.27] flex items-center gap-[6px]"
+      >
+        <span className="furniture-link ">BACK TO TOP</span>
+        <img
+          className="rotate-[-90deg]"
+          src="/images/arrow-right-long.svg"
+        ></img>
+      </button>
+    </section>
+  );
 }
 
-export default FooterTop
+export default FooterTop;
