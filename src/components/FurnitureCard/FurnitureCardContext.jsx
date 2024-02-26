aad1e8eadadac887d68da212b52b78346a3c4487
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-02-26 11:06:51 +0700

    create: add to wishlist

--- src/components/FurnitureCard/FurnitureCardContext.jsx
+++ src/components/FurnitureCard/FurnitureCardContext.jsx
@@ -1,14 +1,25 @@
 import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 import { MODEL_DIMENSION } from "../../constants/enum";
+import { useWishlist } from "../../stores/useWishList";
 
 const FurnitureCardContext = createContext();
 
 function FurnitureCardProvider(props) {
   const { children, furniture } = props;
 
+  const { wishlist, addToWishlist, deleteFromWishlist } = useWishlist();
   const [dimension, setDimension] = useState(MODEL_DIMENSION.two_dimension);
-  const [favored, setFavored] = useState(false);
+  const [favored, setFavored] = useState(
+    wishlist.some((item) => item._id === furniture._id)
+  );
+
+  const onFavored = () => {
+    setFavored(!favored);
+
+    if (!favored) addToWishlist(furniture);
+    if (favored) deleteFromWishlist(furniture._id);
+  };
 
   const onSale = Math.floor(Math.random() * 10) % 2 == 0;
 
@@ -17,7 +28,7 @@ function FurnitureCardProvider(props) {
     dimension,
     setDimension,
     favored,
-    setFavored,
+    onFavored,
     onSale,
   };
 
--- src/pages/Wishlist/index.jsx
+++ src/pages/Wishlist/index.jsx
@@ -1,23 +1,52 @@
-import WebServices from "@components/WebServices"
+import WebServices from "@components/WebServices";
+import { useWishlist } from "../../stores/useWishList";
+import FurnitureCard from "../../components/FurnitureCard";
 
 const Wishlist = () => {
+  const items = useWishlist((store) => store.wishlist);
+
+  const HAVE_ITEM = items.length > 0;
   return (
     <main className="flex flex-col items-center">
-        <section className="block p-28">
-            <h1 className="uppercase text-5xl font-[900] text-center tracking-widest">FAVOURITES</h1>
+      <section className="block p-28">
+        <h1 className="uppercase text-5xl font-[900] text-center tracking-widest">
+          FAVORITES
+        </h1>
+      </section>
+
+      {HAVE_ITEM && (
+        <section className="border-gray-400 border-t border-b w-3/4 py-6 mb-4">
+          <p className="text-center">
+            Do you want to share your favorites with someone else?{" "}
+            <a className="underline" href="#">
+              Login
+            </a>
+          </p>
         </section>
+      )}
 
-        {/* If wishlist has item then show this */}
-        <section className="border-gray-400 border-t border-b w-3/4 py-6 mb-4 hidden">
-            <p className="text-center">Do you want to share your favourites with someone else? <a className="underline" href="#">Login</a></p>
-        </section>
+      {/* Wishlist items */}
+      <div className="grid grid-cols-3">
+        {items.map((item) => {
+          const { _id } = item;
+          return (
+            <FurnitureCard item={item} key={_id}>
+              <FurnitureCard.Model className="w-[60%]">
+                <FurnitureCard.Favorite />
+                <FurnitureCard.DimensionOption />
+              </FurnitureCard.Model>
+              <div className="px-[18px] flex flex-col justify-between gap-4">
+                <FurnitureCard.Attribute />
+                <FurnitureCard.Price />
+              </div>
+            </FurnitureCard>
+          );
+        })}
+      </div>
 
-        {/* Wishlist items */}
-       
-
-        <WebServices />
+      <WebServices />
     </main>
-  )
-}
+  );
+};
 
-export default Wishlist
+export default Wishlist;
