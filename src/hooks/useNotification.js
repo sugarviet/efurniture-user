fdeea265bbebf3841f526635b79e01b6d5e9b7c1
Viet Dang <vietdanghoang1705@gmail.com>
2024-02-29 08:20:06 +0700

    feat: handle create use notification

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -1,10 +1,30 @@
 import { message } from "antd";
+
+
+const type = {
+  address: {
+    success: {
+      add: 'Successfully added address',
+      edit: 'Successfully edited address',
+      delete: 'Successfully deleted delete',
+      set_default: 'Successfully set default address',
+    },
+    fail:{
+      add: 'Failed added address',
+      edit: 'Failed edited address',
+      delete: 'Failed deleted delete',
+      set_default: 'Failed set default address',
+    }
+  }
+}
+
 function useNotification() {
-  const success_message = (msg) => {
-    message.success(msg);
+  const success_message = (msg, action, custom) => {
+    message.success(custom ? type[msg].success[action]: custom);
   };
-  const error_message = (msg) => {
-    message.error(msg);
+  const error_message = (msg, action, custom) => {
+    message.success(custom ? type[msg].fail[action] : custom);
+
   };
   return {
     success_message,
