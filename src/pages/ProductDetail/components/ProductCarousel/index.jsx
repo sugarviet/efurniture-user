40c7c3cd3ae1a8d3fa81dbaea3ff0dcc6987b039
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-03-01 08:58:25 +0700

    Update index.jsx

--- src/pages/ProductDetail/components/ProductCarousel/index.jsx
+++ src/pages/ProductDetail/components/ProductCarousel/index.jsx
@@ -1,23 +1,5 @@
 import Sliders from "@components/Slider";
 
-const productImageDetail = [
-]
-  {
-    img: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dwce7ff147/images/2070000/2072649.jpg?sw=2000",
-  },
-  {
-    img: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc4372027/images/2070000/2072646.jpg?sw=2000",
-  },
-  {
-    img: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw9b987dee/images/2080000/2082878.jpg?sfrm=jpg",
-  },
-  {
-    img: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dw29c950f7/images/2080000/2082536.jpg?sw=2000",
-  },
-  {
-    img: "https://images.demandware.net/dw/image/v2/BBBV_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc854715e/images/2100000/2105829.jpg?sw=2000",
-  },
-];
 function ProductCarousel({ thumbs }) {
   return (
     <section className="w-full bg-white cursor-zoom-in">
