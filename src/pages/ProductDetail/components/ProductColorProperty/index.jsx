cb0ba8344408eda4a32bc4c59b128cbbc7c55e5d
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-04-17 22:24:50 +0700

    Update index.jsx

--- src/pages/ProductDetail/components/ProductColorProperty/index.jsx
+++ src/pages/ProductDetail/components/ProductColorProperty/index.jsx
@@ -17,15 +17,23 @@ function ProductColorProperty({
         onSelectProperty(property);
       }}
       className={classNames(
-        "border w-fit p-2",
-        selectProperty === _id ? "border-black" : "border-gray-300",
-        outOfStock ? "opacity-10" : ""
+        "border w-fit p-2 relative",
+        selectProperty === _id ? "border-black" : "border-gray-300"
       )}
     >
       <div
         style={{ backgroundColor: value }}
         className={`rounded-full w-10 h-10 border ${className}`}
       ></div>
+      {outOfStock && (
+        <div className="absolute top-0 left-0 right-0 bottom-0 z-10 bg-white opacity-70 flex items-center justify-center">
+          <div className="border border-dashed border-black shadow-xl -rotate-45 flex items-center justify-center">
+            <span className="uppercase text-[8px] font-bold tracking-widest">
+              out of stock
+            </span>
+          </div>
+        </div>
+      )}
     </button>
   );
 }
