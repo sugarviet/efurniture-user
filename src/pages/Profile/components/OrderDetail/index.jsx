013138647607bc7fc36fefbb93772f220b810ec4
truonggiang0405 <vutruonggiang452002@gmail.com>
2024-04-21 23:40:47 +0700

    hulk update

--- src/pages/Profile/components/OrderDetail/index.jsx
+++ src/pages/Profile/components/OrderDetail/index.jsx
@@ -9,7 +9,6 @@ import ProductOrderBriefInfo from "../ProductOrderBriefInfo";
 import DepositPrice from "../../../../components/DepositPrice";
 
 function OrderDetail({ data }) {
-    console.log(data);
 
     const { go_to_back } = useNavigation();
 
