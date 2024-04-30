630cdbf242d5b8783606a79699e82b65da414a25
nobi-onway <115331625+nobita-onway@users.noreply.github.com>
2024-04-30 16:13:44 +0700

    update

--- src/api/furnitureTypeApi.js
+++ src/api/furnitureTypeApi.js
@@ -1,7 +1,13 @@
 const FURNITURE_TYPE_URL = "/type";
+const FURNITURE_SUB_TYPE_URL = '/subType'
 
 export const get_furniture_type_api = () => FURNITURE_TYPE_URL;
 
 export const get_furniture_subtype_api = (slug) => {
     return FURNITURE_TYPE_URL + "/" + slug;
+}
+
+export const get_furniture_subtype_detail_api = (slug) => {
+    const { type, subtype } = slug;
+    return FURNITURE_SUB_TYPE_URL + "/" + type + "/" + subtype;
 }
